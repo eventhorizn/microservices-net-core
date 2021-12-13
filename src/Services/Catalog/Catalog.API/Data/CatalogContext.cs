bbc1a861d9eabf7096a6ba64b5c61e8bff17c1f2
Gary Hake <garyhake@gmail.com>
2021-12-12 20:43:53 -0500

    Data Layer stuff for product

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public class CatalogContext : ICatalogContext
+{
+    public IMongoCollection<Product> Products { get; }
+
+    public CatalogContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var db = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+        Products = db.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        //CatalogContext.SeedData(Products);
+    }
+}
