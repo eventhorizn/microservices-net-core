962270ad3b4ad306eb713b08f0785d016f5ae287
Gary Hake <garyhake@gmail.com>
2021-12-17 14:02:14 -0500

    discount grpc files

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,6 +1,5 @@
 using Discount.Grpc.Extensions;
 using Discount.Grpc.Repositories;
-using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +15,7 @@ builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 app.MigrateDatabase<Program>();
 
